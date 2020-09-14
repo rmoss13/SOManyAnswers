b006d69ea29ffc28ff593db667c4d37ba2748974
Riley Moss <riley.moss@accenture.com>
2020-09-14 18:02:53 -0500

    Add structure for question list fragment

--- app/src/main/java/com/rmoss/somanyanswers/MainActivity.kt
+++ app/src/main/java/com/rmoss/somanyanswers/MainActivity.kt
@@ -7,5 +7,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment = QuestionFragment()
+        fragmentTransaction.add(R.id.fragment_container, fragment)
+        fragmentTransaction.commit()
+
     }
 }
